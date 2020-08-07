c4bad6fb6c9c20b960681306fbe337a96bf5f3e8
Haotian Bai <164140757@qq.com>
2020-08-07 20:11:15 +0800

    DK finish

--- src/test/java/Algorithms/Graph/Dynamic/DKTest.java
+++ src/test/java/Algorithms/Graph/Dynamic/DKTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 class DKTest {
     private UndirectedGraph<String, DefaultEdge> graph;
@@ -22,9 +23,13 @@ class DKTest {
     void run() {
         UndirectedGraph<String,DefaultEdge> src = new UndirectedGraph<>(DefaultEdge.class);
         src.addVertex("F");
+        src.addVertex("C");
         DK<String,DefaultEdge> dk = new DK<>(src,graph,0.5) ;
         dk.run();
         StatisticsMatrix res = dk.getResult();
+        // normalize to percentage
+        double sum = res.elementSum();
+        Arrays.stream(res.data()).forEach(e->System.out.printf("%f.2\n",e/sum*100));
     }
 
     @Test
