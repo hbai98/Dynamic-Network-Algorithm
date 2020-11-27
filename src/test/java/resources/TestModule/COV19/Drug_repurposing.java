22f5bccfc040266f121c26bf554d809cb4c7db8a
164140757 <164140757@qq.com>
2020-11-27 15:55:07 +0800

    update

---
+++ src/test/java/resources/TestModule/COV19/Drug_repurposing.java
@@ -0,0 +1,15 @@
+package resources.TestModule.COV19;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Drug_repurposing {
+    @BeforeEach
+    void setup(){
+
+    }
+    @Test
+    void find_drug_by_hga(){
+        // propagate
+    }
+}
